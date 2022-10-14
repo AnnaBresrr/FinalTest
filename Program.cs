21c276e834555699e41b69570326d2c84f801a12
Bresler Anna <bresleranna@yandex.ru>
2022-10-14 23:15:31 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        // string[] values = {"hello", "world", ":-)"};
+         string[] values = {"1234", "1567", "-2", "computer science"};
+        // string[] values = {"Russia", "Denmark", "Kazan"};
+
+        string[] result = Array.FindAll(values, i => i.Length <= 3);
+
+        foreach (string item in result) {
+            Console.WriteLine(item);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 ## Scheme
 
 ![scheme](scheme.svg)
-FinalTest
